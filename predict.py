1a3385af2360be9f7b9bdda720122867a68e7772
Dhia_Oussayed <75687069+Dhiaeddine-Oussayed@users.noreply.github.com>
2022-05-26 13:27:52 +0100

    create predict.py

--- predict.py
+++ predict.py
@@ -1,1 +1,8 @@
-
+def predict(image_path, model, encoder):
+  image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+  resized = cv2.resize(image, image_shape[:2])
+  prediction = model.predict(resized.reshape(-1, img_size, img_size, img_dimension))
+  label_encoded = [np.argmax(prediction)]
+  label = encoder.inverse_transform(label_encoded)[0]
+  print("Predicted label: ",label)
+  cv2_imshow(image)
